0df17e6b127e65c3a9da9ed42b84c57dc6f8e107
Red Leicester Cheese <red_leicester_cheese@outlook.com>
2025-06-19 22:10:26 +0100

    Condense the rundown selector class into 2 classes (R8 selector is a special guy)

--- ExtraRundownCustomization/DataHolders/CustomRundownSelections.cs
+++ ExtraRundownCustomization/DataHolders/CustomRundownSelections.cs
@@ -6,215 +6,226 @@
         public bool Enabled { get; set; } = false;
         public string TextHeader { get; set; } = "SELECT RUNDOWN";
         public string TextHeaderTop { get; set; } = "<color=red>CLCTR MULTITHREAD PROCESSOR ACTIVATED";
-        public Selection_R1 Selector_R1 { get; set; }
-        public Selection_R2 Selector_R2 { get; set; }
-        public Selection_R3 Selector_R3 { get; set; }
-        public Selection_R4 Selector_R4 { get; set; }
-        public Selection_R5 Selector_R5 { get; set; }
-        public Selection_R6 Selector_R6 { get; set; }
-        public Selection_R7 Selector_R7 { get; set; }
+
+        public RundownSelector Selector_R1 { get; set; } = new RundownSelector
+        {
+            name = "R1",
+            altText = "<color=orange>ALT://",
+
+            posX = -476.0f,
+            posY = 45.0f,
+            posZ = 0.0f,
+
+            rotX = 0.0f,
+            rotY = 0.0f,
+            rotZ = 0.0f,
+
+            scaleX = 0.6f,
+            scaleY = 0.6f,
+            scaleZ = 0.6f,
+
+            namePosX = 0.0f,
+            namePosY = 39.0f,
+            namePosZ = 0.0f,
+
+            altTextPosX = -46.0f,
+            altTextPosY = 34.8f,
+            altTextPosZ = 0.0f
+        };
+
+        public RundownSelector Selector_R2{ get; set; } = new RundownSelector()
+        {
+            name = "R2",
+            altText = "<color=orange>ALT://",
+
+            posX = -241f,
+            posY = -78f,
+            posZ = 0f,
+
+            rotX = 0f,
+            rotY = 0f,
+            rotZ = 0f,
+
+            scaleX = 0.8f,
+            scaleY = 0.8f,
+            scaleZ = 0.8f,
+
+            namePosX = 0f,
+            namePosY = 39f,
+            namePosZ = 0f,
+
+            altTextPosX = -46f,
+            altTextPosY = 34.8f,
+            altTextPosZ = 0f
+        };
+
+        public RundownSelector Selector_R3 { get; set; } = new RundownSelector
+        {
+            name = "R3",
+            altText = "<color=orange>ALT://",
+
+            posX = -346f,
+            posY = 179.6f,
+            posZ = 13.956f,
+
+            rotX = 0f,
+            rotY = 0f,
+            rotZ = 0f,
+
+            scaleX = 0.8f,
+            scaleY = 0.8f,
+            scaleZ = 0.8f,
+
+            namePosX = 0f,
+            namePosY = 39f,
+            namePosZ = 0f,
+
+            altTextPosX = -46f,
+            altTextPosY = 34.8f,
+            altTextPosZ = 0f
+        };
+
+        public RundownSelector Selector_R4 { get; set; } = new RundownSelector
+        {
+            name = "R4",
+            altText = "<color=orange>ALT://",
+
+            posX = -109f,
+            posY = 226f,
+            posZ = 0f,
+
+            rotX = 0f,
+            rotY = 0f,
+            rotZ = 0f,
+
+            scaleX = 1.2f,
+            scaleY = 1.2f,
+            scaleZ = 1.2f,
+
+            namePosX = 0f,
+            namePosY = 39f,
+            namePosZ = 0f,
+
+            altTextPosX = -46f,
+            altTextPosY = 34.8f,
+            altTextPosZ = 0f
+        };
+
+        public RundownSelector Selector_R5 { get; set; } = new RundownSelector
+        {
+            name = "R5",
+            altText = "<color=orange>ALT://",
+
+            posX = 55f,
+            posY = -16f,
+            posZ = 0f,
+
+            rotX = 0f,
+            rotY = 0f,
+            rotZ = 0f,
+
+            scaleX = 1.1f,
+            scaleY = 1.1f,
+            scaleZ = 1.1f,
+
+            namePosX = 0f,
+            namePosY = 39f,
+            namePosZ = 0f,
+
+            altTextPosX = -46f,
+            altTextPosY = 34.8f,
+            altTextPosZ = 0f
+        };
+
+        public RundownSelector Selector_R6 { get; set; } = new RundownSelector
+        {
+            name = "R6",
+            altText = "<color=orange>ALT://",
+
+            posX = 240f,
+            posY = 235f,
+            posZ = 0f,
+
+            rotX = 0f,
+            rotY = 0f,
+            rotZ = 0f,
+
+            scaleX = 1.09f,
+            scaleY = 1.09f,
+            scaleZ = 1.09f,
+
+            namePosX = 0f,
+            namePosY = 39f,
+            namePosZ = 0f,
+
+            altTextPosX = -46f,
+            altTextPosY = 34.8f,
+            altTextPosZ = 0f
+        };
+
+        public RundownSelector Selector_R7 { get; set; } = new RundownSelector
+        {
+            name = "R7",
+            altText = "<color=orange>ALT://",
+
+            posX = 317f,
+            posY = -104f,
+            posZ = 0f,
+
+            rotX = 0f,
+            rotY = 0f,
+            rotZ = 0f,
+
+            scaleX = 1f,
+            scaleY = 1f,
+            scaleZ = 1f,
+
+            namePosX = 0f,
+            namePosY = 39f,
+            namePosZ = 0f,
+
+            altTextPosX = -46f,
+            altTextPosY = 34.8f,
+            altTextPosZ = 0f
+        };
         public Selection_R8 Selector_R8 { get; set; }
 
         public void InitializeDefaults()
         {
-            Selector_R1 = new();
-            Selector_R2 = new();
-            Selector_R3 = new();
-            Selector_R4 = new();
-            Selector_R5 = new();
-            Selector_R6 = new();
-            Selector_R7 = new();
+            
             Selector_R8 = new();
         }
     }
 
-    public class Selection_R1
+    public class RundownSelector
     {
-        public string name { get; set; } = "R1";
-        public string altText { get; set; } = "<color=orange>ALT://";
+        public string name { get; set; }
+        public string altText { get; set; }
 
 
-        public float posX { get; set; } = -476.0f;
-        public float posY { get; set; } = 45.0f;
-        public float posZ { get; set; } = 0.0f;
+        public float posX { get; set; }
+        public float posY { get; set; }
+        public float posZ { get; set; }
 
-        public float rotX { get; set; } = 0.0f;
-        public float rotY { get; set; } = 0.0f;
-        public float rotZ { get; set; } = 0.0f;
+        public float rotX { get; set; }
+        public float rotY { get; set; }
+        public float rotZ { get; set; }
 
-        public float scaleX { get; set; } = 0.6f;
-        public float scaleY { get; set; } = 0.6f;
-        public float scaleZ { get; set; } = 0.6f;
+        public float scaleX { get; set; }
+        public float scaleY { get; set; }
+        public float scaleZ { get; set; }
 
-        public float namePosX { get; set; } = 0.0f;
-        public float namePosY { get; set; } = 39.0f;
-        public float namePosZ { get; set; } = 0.0f;
+        public float namePosX { get; set; }
+        public float namePosY { get; set; }
+        public float namePosZ { get; set; }
 
-        public float altTextPosX { get; set; } = -46.0f;
-        public float altTextPosY { get; set; } = 34.8f;
-        public float altTextPosZ { get; set; } = 0.0f;
+        public float altTextPosX { get; set; }
+        public float altTextPosY { get; set; }
+        public float altTextPosZ { get; set; }
     }
-
-    public class Selection_R2
-    {
-        public string name { get; set; } = "R2";
-        public string altText { get; set; } = "<color=orange>ALT://";
-
-        public float posX { get; set; } = -241f;
-        public float posY { get; set; } = -78f;
-        public float posZ { get; set; } = 0f;
-
-        public float rotX { get; set; } = 0f;
-        public float rotY { get; set; } = 0f;
-        public float rotZ { get; set; } = 0f;
-
-        public float scaleX { get; set; } = 0.8f;
-        public float scaleY { get; set; } = 0.8f;
-        public float scaleZ { get; set; } = 0.8f;
-
-        public float namePosX { get; set; } = 0f;
-        public float namePosY { get; set; } = 39f;
-        public float namePosZ { get; set; } = 0f;
-
-        public float altTextPosX { get; set; } = -46f;
-        public float altTextPosY { get; set; } = 34.8f;
-        public float altTextPosZ { get; set; } = 0f;
-    }
-
-    public class Selection_R3
-    {
-        public string name { get; set; } = "R3";
-        public string altText { get; set; } = "<color=orange>ALT://";
-
-        public float posX { get; set; } = -346f;
-        public float posY { get; set; } = 179.6f;
-        public float posZ { get; set; } = 13.956f;
-
-        public float rotX { get; set; } = 0f;
-        public float rotY { get; set; } = 0f;
-        public float rotZ { get; set; } = 0f;
-
-        public float scaleX { get; set; } = 0.8f;
-        public float scaleY { get; set; } = 0.8f;
-        public float scaleZ { get; set; } = 0.8f;
-
-        public float namePosX { get; set; } = 0f;
-        public float namePosY { get; set; } = 39f;
-        public float namePosZ { get; set; } = 0f;
-
-        public float altTextPosX { get; set; } = -46f;
-        public float altTextPosY { get; set; } = 34.8f;
-        public float altTextPosZ { get; set; } = 0f;
-    }
-
-    public class Selection_R4
-    {
-        public string name { get; set; } = "R4";
-        public string altText { get; set; } = "<color=orange>ALT://";
-
-        public float posX { get; set; } = -109f;
-        public float posY { get; set; } = 226f;
-        public float posZ { get; set; } = 0f;
-
-        public float rotX { get; set; } = 0f;
-        public float rotY { get; set; } = 0f;
-        public float rotZ { get; set; } = 0f;
-
-        public float scaleX { get; set; } = 1.2f;
-        public float scaleY { get; set; } = 1.2f;
-        public float scaleZ { get; set; } = 1.2f;
-
-        public float namePosX { get; set; } = 0f;
-        public float namePosY { get; set; } = 39f;
-        public float namePosZ { get; set; } = 0f;
-
-        public float altTextPosX { get; set; } = -46f;
-        public float altTextPosY { get; set; } = 34.8f;
-        public float altTextPosZ { get; set; } = 0f;
-    }
-
-    public class Selection_R5
-    {
-        public string name { get; set; } = "R5";
-        public string altText { get; set; } = "<color=orange>ALT://";
-
-        public float posX { get; set; } = 55f;
-        public float posY { get; set; } = -16f;
-        public float posZ { get; set; } = 0f;
-
-        public float rotX { get; set; } = 0f;
-        public float rotY { get; set; } = 0f;
-        public float rotZ { get; set; } = 0f;
-
-        public float scaleX { get; set; } = 1.1f;
-        public float scaleY { get; set; } = 1.1f;
-        public float scaleZ { get; set; } = 1.1f;
-
-        public float namePosX { get; set; } = 0f;
-        public float namePosY { get; set; } = 39f;
-        public float namePosZ { get; set; } = 0f;
-
-        public float altTextPosX { get; set; } = -46f;
-        public float altTextPosY { get; set; } = 34.8f;
-        public float altTextPosZ { get; set; } = 0f;
-    }
-
-    public class Selection_R6
-    {
-        public string name { get; set; } = "R6";
-        public string altText { get; set; } = "<color=orange>ALT://";
-
-        public float posX { get; set; } = 240f;
-        public float posY { get; set; } = 235f;
-        public float posZ { get; set; } = 0f;
-
-        public float rotX { get; set; } = 0f;
-        public float rotY { get; set; } = 0f;
-        public float rotZ { get; set; } = 0f;
-
-        public float scaleX { get; set; } = 1.09f;
-        public float scaleY { get; set; } = 1.09f;
-        public float scaleZ { get; set; } = 1.09f;
-
-        public float namePosX { get; set; } = 0f;
-        public float namePosY { get; set; } = 39f;
-        public float namePosZ { get; set; } = 0f;
-
-        public float altTextPosX { get; set; } = -46f;
-        public float altTextPosY { get; set; } = 34.8f;
-        public float altTextPosZ { get; set; } = 0f;
-    }
-
-    public class Selection_R7
-    {
-        public string name { get; set; } = "R7";
-        public string altText { get; set; } = "<color=orange>ALT://";
-
-        public float posX { get; set; } = 317f;
-        public float posY { get; set; } = -104f;
-        public float posZ { get; set; } = 0f;
-
-        public float rotX { get; set; } = 0f;
-        public float rotY { get; set; } = 0f;
-        public float rotZ { get; set; } = 0f;
-
-        public float scaleX { get; set; } = 1f;
-        public float scaleY { get; set; } = 1f;
-        public float scaleZ { get; set; } = 1f;
-
-        public float namePosX { get; set; } = 0f;
-        public float namePosY { get; set; } = 39f;
-        public float namePosZ { get; set; } = 0f;
-
-        public float altTextPosX { get; set; } = -46f;
-        public float altTextPosY { get; set; } = 34.8f;
-        public float altTextPosZ { get; set; } = 0f;
-    }
-
     public class Selection_R8
     {
         public string name { get; set; } = "R8";
-        public string altText { get; set; } = "<color=orange>ALT://";
 
         public float posX { get; set; } = 550f;
         public float posY { get; set; } = 150f;
@@ -231,9 +242,5 @@
         public float namePosX { get; set; } = 0f;
         public float namePosY { get; set; } = 39f;
         public float namePosZ { get; set; } = 0f;
-
-        public float altTextPosX { get; set; } = -46f;
-        public float altTextPosY { get; set; } = 34.8f;
-        public float altTextPosZ { get; set; } = 0f;
     }
 }
